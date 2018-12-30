e16693be738e869ceb0d921096aaa27f9949331f
ReyMerk <furbino1@gmail.com>
2018-12-30 12:12:09 +0100

    Corrected callback for last cluster loaded
    Callback when supposedly the last cluster has loaded. Corrected, the last doesn't work

--- clusterize.js
+++ clusterize.js
@@ -235,6 +235,9 @@
       if(top_offset < 1) {
         rows_above++;
       }
+      if(items_end > rows_len)
+         if (opts.callbacks.last_cluster_load)
+            opts.callbacks.last_cluster_load();
       for (var i = items_start; i < items_end; i++) {
         rows[i] && this_cluster_rows.push(rows[i]);
       }
